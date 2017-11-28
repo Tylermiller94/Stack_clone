e910e57b64d3bc2ba65a32b1c5592d0f8904eb57
Gennaro Anzalone and Tyler Miller <tyler@>
2017-11-28 10:07:58 -0800

    add one to many relationship btwn questions and answers

---
+++ db/migrate/20171128180343_add_column_answers.rb
@@ -0,0 +1,5 @@
+class AddColumnAnswers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :questions, :answer_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128175134) do
+ActiveRecord::Schema.define(version: 20171128180343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20171128175134) do
     t.text "question"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "answer_id"
   end
 
   create_table "users", force: :cascade do |t|
